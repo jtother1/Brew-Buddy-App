c0d7b22297f10ffb94285d017ad92acd8b83b3f3
jtother <juanramonquiroz15@gmail.com>
2020-05-04 08:58:42 -0500

    added address to display at my results

--- src/components/BreweryResult.js
+++ src/components/BreweryResult.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 
 class BreweryResult extends Component {
   render() {
+    //just makes things clearer by eliminating this.props
     const { brewery, saved, toggleSave } = this.props;
+    //all the info displayed in search results
     return (
       <div>
         <h3>{brewery.name}</h3>
         <p>Brewery Type: {brewery.brewery_type}</p>
+        <p>Address: {brewery.street}</p>
         <p>
           {brewery.city}, {brewery.state}
         </p>
-        <p>{brewery.street}</p>
         <p>{brewery.website_url}</p>
         <input onChange={toggleSave} type="checkbox" checked={saved} />
       </div>
